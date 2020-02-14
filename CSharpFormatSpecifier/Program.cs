c605eb805fe72e64d7b1c6d9202d15bf491abca5
Rajdeep Debnath <debnath.rajdeep@gmail.com>
2020-02-15 05:27:43 +0530

    Currency and exponential format specifier

--- CSharpFormatSpecifier/Program.cs
+++ CSharpFormatSpecifier/Program.cs
@@ -7,9 +7,18 @@ namespace CSharpFormatSpecifier
     {
         static void Main(string[] args)
         {
-            var ci = new CultureInfo("en-US");
-            var a = 1234;
-            Console.WriteLine(a.ToString("C", ci));
+            var ci = new CultureInfo("en-GB");
+            var a = 1234.567;
+            Console.WriteLine(a.ToString("C4", ci));
+            Console.WriteLine(a.ToString("C0", ci));
+            Console.WriteLine(a.ToString("C1", ci));
+            Console.WriteLine(a.ToString("C3", ci));
+
+
+            a = 3236;
+            Console.WriteLine(a.ToString("E"));
+            Console.WriteLine(a.ToString("E1"));
+            Console.WriteLine(a.ToString("E3"));
             Console.WriteLine("done...");
         }
     }
