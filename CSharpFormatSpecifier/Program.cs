44ed6947e010d321fc1bae8e14f362e1d41653f3
Rajdeep Debnath <debnath.rajdeep@gmail.com>
2020-02-15 02:02:57 +0530

    initial commit

---
+++ CSharpFormatSpecifier/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace CSharpFormatSpecifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ci = new CultureInfo("en-US");
+            var a = 1234;
+            Console.WriteLine(a.ToString("C", ci));
+            Console.WriteLine("done...");
+        }
+    }
+}
